ead760606dfb9e411d440ae7539b7dd7c64eca8e
Greyson Parrelli <greyson.parrelli@gmail.com>
2016-05-14 17:08:04 -0700

    Can send a gif.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/sender', function(req, res){
+  res.sendFile(__dirname + '/sender.html');
+});
+
+app.get('/receiver', function(req, res){
+  res.sendFile(__dirname + '/receiver.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('send_reaction', function(msg){
+    console.log('received send');
+    socket.broadcast.emit('receive_reaction', msg);
+  });
 });
 
 var port = process.env.PORT || 8000;
-app.listen(port, function () {
-  console.log('React IRL listening on port '  + port + '!');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
